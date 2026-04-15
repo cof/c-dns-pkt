603aa338fa2e63ef551ebb7970304d9119ccbf46
cof <cof@fastmail.fm>
2026-04-15 20:14:20 +0100

    Updated exit-code

--- test-integration.sh
+++ test-integration.sh
@@ -7,8 +7,9 @@ GEN_RESP="$GEN_NAME resp"
 GEN_FUZZ="$GEN_NAME fuzz"
 CHK_RESP="$INSPECT_NAME readpcap --file"
 TEST_PCAP="$BUILD_DIR/test.pcap"
-BAD_PDU=6
-BAD_LABEL=5
+# exit codes 
+BAD_PDU=5
+BAD_LABEL=4
 mkdir -p ${BUILD_DIR} || exit 1
 
 TEST_NAME="Simple A query"
